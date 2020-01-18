319bb2ffdef2489d4740c751c11afb0e961b4cb1
Vladislav Zhabinsky <vladislavzhabinsky@gmail.com>
2020-01-18 21:48:44 +0200

    added props types

--- docs-src/src/index.js
+++ docs-src/src/index.js
@@ -44,106 +44,177 @@ const CodeExample = example => {
   );
 };
 
-render (
-  <React.Fragment>
-    <h1
-      style={{
-        fontWeight: 900,
-        fontSize: 50,
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-      }}
-    >
-      React Bloated Grid 🐒
-      <a
-        href="https://github.com/zhabinsky"
+const App = () => {
+  const propsString = `const CustomTypes = {
+  NumberOrBreakpointObject: PropTypes.oneOfType ([
+    PropTypes.number,
+    PropTypes.object,
+  ]),
+  BoolOrBreakpointObject: PropTypes.oneOfType ([
+    PropTypes.bool,
+    PropTypes.object,
+  ]),
+};
+
+const propTypes = {
+  id: PropTypes.string, // auto-generated
+  children: PropTypes.arrayOf (PropTypes.node),
+  style: PropTypes.object,
+  styleChild: PropTypes.object,
+
+  gridColumns: CustomTypes.NumberOrBreakpointObject.isRequired,
+  gridGap: CustomTypes.NumberOrBreakpointObject,
+  gridRowGap: CustomTypes.NumberOrBreakpointObject,
+  gridColumnGap: CustomTypes.NumberOrBreakpointObject,
+
+  className: PropTypes.string,
+  classNameChild: PropTypes.string,
+  classNameChildSelected: PropTypes.string,
+
+  trimLastRow: CustomTypes.BoolOrBreakpointObject, // makes there are no empty slots in rows
+
+  effectScale: CustomTypes.NumberOrBreakpointObject,
+  effectScaleMovement: CustomTypes.NumberOrBreakpointObject,
+  effectScaleMagnification: CustomTypes.NumberOrBreakpointObject,
+
+  disableMagnification: CustomTypes.BoolOrBreakpointObject,
+  disableMovement: CustomTypes.BoolOrBreakpointObject,
+};
+
+const defaultProps = {
+  id: undefined,
+  children: [],
+  style: {},
+  styleChild: {
+    transition: 'all 0.4s ease-out',
+  },
+
+  gridColumns: 5,
+  gridGap: 20,
+  gridRowGap: 20,
+  gridColumnGap: 20,
+
+  className: '',
+  classNameChild: '',
+  classNameChildSelected: '',
+
+  trimLastRow: false,
+
+  effectScale: 1,
+  effectScaleMovement: 1,
+  effectScaleMagnification: 1,
+
+  disableMagnification: false,
+  disableMovement: false,
+};`;
+  return (
+    <React.Fragment>
+      <h1
         style={{
-          color: 'rgba(255,255,255,0.4)',
-          textDecoration: 'none',
-          fontSize: 30,
+          fontWeight: 900,
+          fontSize: 50,
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
         }}
       >
-        zhabinsky@github
+        React Bloated Grid 🐒
+        <a
+          href="https://github.com/zhabinsky"
+          style={{
+            color: 'rgba(255,255,255,0.4)',
+            textDecoration: 'none',
+            fontSize: 30,
+          }}
+        >
+          zhabinsky@github
+        </a>
+      </h1>
+
+      <a
+        href="https://github.com/zhabinsky/react-bloating-grid"
+        style={{
+          fontSize: 20,
+          color: 'white',
+          paddingTop: 40,
+          marginBottom: 40,
+        }}
+      >
+        Github repository
       </a>
-    </h1>
+      <br />
+      <a
+        href="https://www.npmjs.com/package/react-bloating-grid"
+        style={{
+          fontSize: 20,
+          color: 'white',
+          paddingTop: 40,
+          marginBottom: 40,
+        }}
+      >
+        nmpjs package
+      </a>
 
-    <a
-      href="https://github.com/zhabinsky/react-bloating-grid"
-      style={{
-        fontSize: 20,
-        color: 'white',
-        paddingTop: 40,
-        marginBottom: 40,
-      }}
-    >
-      Github repository
-    </a>
-    <br />
-    <a
-      href="https://github.com/zhabinsky/react-bloating-grid"
-      style={{
-        fontSize: 20,
-        color: 'white',
-        paddingTop: 40,
-        marginBottom: 40,
-      }}
-    >
-      nmpjs package
-    </a>
+      <p style={{margin: '50px 0', fontSize: 16}}>
+        <h3>
+          Easily create beautiful, responsive, performant content grids, that:
+        </h3>
+        <ul>
+          <li>
+            react to hover event by pushing the neighbouring elements aside
+          </li>
+          <li>allow you to tune its behaviour via a number of props</li>
+          <li>support responsive props</li>
+        </ul>
+      </p>
 
-    <p style={{margin: '50px 0', fontSize: 16}}>
-      <h3>
-        Easily create beautiful, responsive, performant content grids, that:
-      </h3>
-      <ul>
-        <li>react to hover event by pushing the neighbouring elements aside</li>
-        <li>allow you to tune its behaviour via a number of props</li>
-        <li>support responsive props</li>
-      </ul>
-    </p>
+      <GridWrapper>
+        <BloatingGrid gridColumns={6}>
+          {repeat (6)}
+        </BloatingGrid>
+      </GridWrapper>
 
-    <GridWrapper>
-      <BloatingGrid gridColumns={6}>
-        {repeat (6)}
-      </BloatingGrid>
-    </GridWrapper>
+      <Title title={'PropTypes'}>
+        <Code code={propsString}> </Code>
+      </Title>
 
-    <div style={{marginBottom: 100}}>
-      {[
-        {
-          title: 'Simple row',
-          n: 6,
-          componentProps: {
-            gridColumns: 6,
-          },
-        },
-        {
-          title: 'Multi dimensional',
-          n: 6 * 3,
-          componentProps: {
-            gridColumns: 6,
-          },
-        },
-        {
-          title: 'Responsive props',
-          n: 10,
-          componentProps: {
-            trimLastRow: true,
-            gridColumns: {
-              '0': 3,
-              '512': 4,
-              '768': 5,
-              '1024': 6,
-              '1500': 7,
+      <div style={{marginBottom: 100}}>
+        {[
+          {
+            title: 'Simple row',
+            n: 6,
+            componentProps: {
+              gridColumns: 6,
             },
           },
-        },
-      ].map (e => <CodeExample {...e} />)}
-    </div>
-  </React.Fragment>,
-  document.getElementById ('app')
-);
+          {
+            title: 'Multi dimensional',
+            n: 6 * 3,
+            componentProps: {
+              gridColumns: 6,
+            },
+          },
+          {
+            title: 'Responsive props',
+            n: 10,
+            componentProps: {
+              trimLastRow: true,
+              gridColumns: {
+                '0': 3,
+                '512': 4,
+                '768': 5,
+                '1024': 6,
+                '1500': 7,
+              },
+            },
+          },
+        ].map (e => <CodeExample {...e} />)}
+      </div>
+    </React.Fragment>
+  );
+};
+
+render (<App />, document.getElementById ('app'));
 
 function random () {
   return 120 + Math.random () * 75;
