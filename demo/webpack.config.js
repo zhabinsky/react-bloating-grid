30812ccc83d50acc213b83b41a6cc43db8769e2a
Vladislav Zhabinsky <vladislavzhabinsky@gmail.com>
2020-01-18 19:51:33 +0200

    No message specified

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -1,19 +1,24 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require ('html-webpack-plugin');
+const path = require ('path');
 
 module.exports = {
-    mode: 'development',
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                exclude: /(node_modules)/,
-                use: 'babel-loader'
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        })
-    ]
-}
+  mode: 'development',
+  output: {
+    // options related to how webpack emits results
+    path: path.resolve (__dirname, '../docs'), // string
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin ({
+      template: './src/index.html',
+    }),
+  ],
+};
